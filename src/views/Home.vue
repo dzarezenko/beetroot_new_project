a326223a567679ec7d9983ba408f347ba7a226fa
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-12-09 20:58:35 +0200

    Tags - components example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hello-world msg="HELLO WORLD!"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorldComponent from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    'hello-world': HelloWorldComponent // tag: component
   }
 }
 </script>
